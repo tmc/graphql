1b12339eb5eebb3da6055eff84e0f7b02a7f025f
Travis Cline <travis.cline@gmail.com>
2015-04-18 16:50:26 -0700

    Add additional example

--- schema/schema_example_test.go
+++ schema/schema_example_test.go
@@ -3,6 +3,7 @@ package schema_test
 import (
 	"encoding/json"
 	"fmt"
+	"time"
 
 	"github.com/tmc/graphql"
 	"github.com/tmc/graphql/parser"
@@ -24,6 +25,40 @@ func ExampleSchema() {
 	// output:
 	// {
 	//  "root_calls": [
+	//   "schema"
+	//  ]
+	// }
+}
+
+type nowProvider struct{}
+
+func (n *nowProvider) now(c graphql.Call) (interface{}, error) {
+	return time.Now(), nil
+}
+
+func (n *nowProvider) RootCalls() map[string]schema.CallHandler {
+	return map[string]schema.CallHandler{
+		"now": n.now,
+	}
+}
+
+func ExampleSchemaCustomType() {
+	s := schema.New()
+	s.Register(new(nowProvider))
+	call, err := parser.Parse("schema_test.go", []byte(`schema(){root_calls}`))
+	if err != nil {
+		fmt.Println(err)
+	}
+	result, err := s.HandleCall(call.(graphql.Call))
+	if err != nil {
+		fmt.Println(err)
+	}
+	asjson, _ := json.MarshalIndent(result, "", " ")
+	fmt.Println(string(asjson))
+	// output:
+	// {
+	//  "root_calls": [
+	//   "now",
 	//   "schema"
 	//  ]
 	// }
