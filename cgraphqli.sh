bf3630979e9273e16c92a962ee93f1a00ab7e17d
Travis Cline <travis.cline@gmail.com>
2015-05-19 15:48:00 -0400

    Add cgraphqli.sh

---
+++ cgraphqli.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# cgraphqli.sh
+#
+# requirements:
+# jq - for output formatting
+# rlwrap - optional requirement for readline support (command history)
+
+set -eou pipefail
+BASEURL=${1:-"http://localhost:8080/"}
+
+function query(){
+	local q=${1:-"{ schema(){ root_calls { name } } }"}
+	echo curl -sgG --data-urlencode "\"q=${q}\"" "${BASEURL}"
+	curl -sgG --data-urlencode "q=${q}" "${BASEURL}" | jq .
+	echo
+}
+
+function maybe_rlwrap() {
+	if [ "${USING_RLWRAP:-0}" -eq "0" ] && which -s rlwrap; then
+		USING_RLWRAP=1 exec rlwrap "$0" "$@"
+	fi
+}
+function main(){
+	maybe_rlwrap "$@"
+	echo "using base url '${BASEURL}'"
+	while read line; do
+		query "$line"
+	done
+}
+
+main "$@"
