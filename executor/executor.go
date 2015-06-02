535b4194d9601cfc23a205c1835c7ba15e0f3aaa
Travis Cline <travis.cline@gmail.com>
2015-06-02 15:24:18 -0700

    Resolve fields and slices in parallel

--- executor/executor.go
+++ executor/executor.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"reflect"
+	"sync"
 
 	"github.com/tmc/graphql"
 	"github.com/tmc/graphql/schema"
@@ -50,6 +51,12 @@ func isSlice(value interface{}) bool {
 	return reflect.TypeOf(value).Kind() == reflect.Slice
 }
 
+type fieldResult struct {
+	FieldName string
+	Value     interface{}
+	Err       error
+}
+
 func (e *Executor) Resolve(ctx context.Context, partial interface{}, field *graphql.Field) (interface{}, error) {
 	if partial != nil && isSlice(partial) {
 		return e.resolveSlice(ctx, partial, field)
@@ -67,24 +74,45 @@ func (e *Executor) Resolve(ctx context.Context, partial interface{}, field *grap
 
 	result := map[string]interface{}{}
 	typeInfo := schema.WithIntrospectionField(graphQLValue.GraphQLTypeInfo())
+	results := make(chan fieldResult)
+	wg := sync.WaitGroup{}
+
 	for _, selection := range field.SelectionSet {
 		fieldName := selection.Field.Name
 		fieldHandler, ok := typeInfo.Fields[fieldName]
 		if !ok {
 			return nil, fmt.Errorf("No handler for field '%s' on type '%T'", fieldName, graphQLValue)
 		}
-		partial, err := fieldHandler.Func(ctx, e, selection.Field)
-		if err != nil {
-			return nil, err // TODO(tmc) decorate error
+		wg.Add(1)
+		go func(selection graphql.Selection) {
+			defer wg.Done()
+			partial, err := fieldHandler.Func(ctx, e, selection.Field)
+			if err != nil {
+				results <- fieldResult{Err: err}
+				return
+			}
+			resolved, err := e.Resolve(ctx, partial, selection.Field)
+			if err != nil {
+				results <- fieldResult{Err: err}
+				return
+			}
+			if selection.Field.Alias != "" {
+				fieldName = selection.Field.Alias
+			}
+			results <- fieldResult{
+				FieldName: fieldName, Value: resolved, Err: err,
+			}
+		}(selection)
+	}
+	go func() {
+		wg.Wait()
+		close(results)
+	}()
+	for r := range results {
+		if r.Err != nil {
+			return nil, r.Err
 		}
-		resolved, err := e.Resolve(ctx, partial, selection.Field)
-		if err != nil {
-			return nil, err
-		}
-		if selection.Field.Alias != "" {
-			fieldName = selection.Field.Alias
-		}
-		result[fieldName] = resolved
+		result[r.FieldName] = r.Value
 	}
 	return result, nil
 }
@@ -92,12 +120,25 @@ func (e *Executor) Resolve(ctx context.Context, partial interface{}, field *grap
 func (e *Executor) resolveSlice(ctx context.Context, partials interface{}, field *graphql.Field) (interface{}, error) {
 	v := reflect.ValueOf(partials)
 	results := make([]interface{}, 0, v.Len())
+	resChan := make(chan fieldResult)
+	wg := sync.WaitGroup{}
 	for i := 0; i < v.Len(); i++ {
-		result, err := e.Resolve(ctx, v.Index(i).Interface(), field)
-		if err != nil {
-			return nil, err
+		wg.Add(1)
+		go func(i int) {
+			defer wg.Done()
+			result, err := e.Resolve(ctx, v.Index(i).Interface(), field)
+			resChan <- fieldResult{Value: result, Err: err}
+		}(i)
+	}
+	go func() {
+		wg.Wait()
+		close(resChan)
+	}()
+	for result := range resChan {
+		if result.Err != nil {
+			return nil, result.Err
 		}
-		results = append(results, result)
+		results = append(results, result.Value)
 	}
 	return results, nil
 }
