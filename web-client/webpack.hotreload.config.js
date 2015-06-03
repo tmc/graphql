e9154e3cd3a3546a2911b8954d75b4c71f67e608
Travis Cline <travis.cline@gmail.com>
2015-06-03 12:05:51 -0500

    Add hot reloading

---
+++ web-client/webpack.hotreload.config.js
@@ -0,0 +1,20 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  entry: [
+    'webpack-dev-server/client?http://localhost:3000',
+    'webpack/hot/only-dev-server',
+    './index.js'
+  ],
+  output: { path: path.join(__dirname, 'dist'), filename: 'app.js' },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ],
+  module: { loaders: [
+    { test: /\.css$/, loader: 'style!css' },
+    { test: /\.scss$/, loader: 'style!sass' },
+    { test: /\.jsx?$/, loaders: ['react-hot', 'babel'], exclude: /node_modules/ }
+  ] }
+};
