4c244090a0d0c129f696d391ae8b42f3c6559505
Travis Cline <travis.cline@gmail.com>
2015-06-18 21:54:47 -0700

    Surface parse errors.

--- parser/parser.go
+++ parser/parser.go
@@ -2,6 +2,7 @@ package parser
 
 import (
 	"errors"
+	"fmt"
 	"log"
 
 	"github.com/tmc/graphql"
@@ -9,28 +10,40 @@ import (
 )
 
 var (
-	// ErrMalformedOperation means there was a parsing error with the provided query
-	ErrMalformedOperation = errors.New("parser: malformed graphql operation")
 	// ErrMultipleOperations
 	ErrMultipleOperations = errors.New("parser: multiple graphql operations")
 )
+
+// ErrMalformedOperation means there was a parsing error with the provided query
+type ErrMalformedOperation struct {
+	underlying error
+}
+
+func IsMalformedOperation(err error) bool {
+	_, ok := err.(ErrMalformedOperation)
+	return ok
+}
+
+func (e ErrMalformedOperation) Error() string {
+	return fmt.Sprintf("parser: malformed graphql operation: %v", e.underlying)
+}
 
 // ParseOperation attempts to parse a graphql.Operation from a byte slice.
 func ParseOperation(query []byte) (*graphql.Operation, error) {
 	result, err := parser.Parse("", query)
 	if err != nil {
 		log.Println("parse error:", err)
-		return nil, ErrMalformedOperation
+		return nil, ErrMalformedOperation{err}
 	}
 	doc, ok := result.(graphql.Document)
 	if !ok {
-		return nil, ErrMalformedOperation
+		return nil, ErrMalformedOperation{err}
 	}
 	switch len(doc.Operations) {
 	case 1:
 		return &doc.Operations[0], nil
 	case 0:
-		return nil, ErrMalformedOperation
+		return nil, ErrMalformedOperation{fmt.Errorf("no operations")}
 	default:
 		return nil, ErrMultipleOperations
 	}
