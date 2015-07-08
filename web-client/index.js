d3b74b4410ae991fbdeda4add82eb4e3b19a12e1
Travis Cline <travis.cline@gmail.com>
2015-07-08 11:04:16 -0600

    separate web-client

--- web-client/index.js
+++
@@ -1,4 +0,0 @@
-import React from 'react';
-import GraphQLWebClientWrapper from './GraphQLWebClientWrapper';
-
-React.render(<GraphQLWebClientWrapper endpoint="http://tmc.parseapp.com/graphql"/>, document.getElementById('root'));
